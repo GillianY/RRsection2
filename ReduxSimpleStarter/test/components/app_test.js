07ca6c2f451498ab5ba8a230e57f70f017185d9e
Gillian <jjjgillian@gmail.com>
2017-10-14 01:20:00 +0800

    5. a first spec
    Mocha can queue these tests up to run in the future so that it can safely execute our code  which might be throwing errors all the time;
    
    mocha https://mochajs.org/
    assertion library :
    except.js : except() style assertions.
    	https://github.com/Automattic/expect.js
    chai.js : except(), assert() and should -style seertions
    	http://chaijs.com/
    
    npm run test:watch

--- ReduxSimpleStarter/test/components/app_test.js
+++ ReduxSimpleStarter/test/components/app_test.js
@@ -1,12 +1,21 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';  // 觀測目標 root
 
-// use 'describe' to group togather similar tests
-describe(' Test the App Component') //scene 
+// use 'describe' to group togather similar tests //scene 
+describe(' Test the App Component', ()=>{
 
-// Use 'it' to test a single attribute of a target  //TestCase 
-it('shows the correct test') //to developers on what this block is goning to test right here 
+  // Use 'it' to test a single attribute of a target  //TestCase 
+  //to developers on what this block is goning to test right here 
+  it('shows the correct test',() =>{
+
+    //create an instance of app 
+    const component = renderComponent(App);
+    //Use 'expect' to make 'assertion' about a target (here: App)
+    expect(component).to.contain('React simple starter');
+
+  }); 
+
+}); 
 
 
-//Use 'expect' to make 'assertion' about a target (here: App)
-expect
+
