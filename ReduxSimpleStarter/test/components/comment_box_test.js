0869f37ca33f4ef699d0bd9d35c1445a3d45c6aa
Gillian <jjjgillian@gmail.com>
2017-10-17 17:50:41 +0800

    8. Add component
    6. Test reporting 看看錯誤的畫面
    7. Feature mockup   //mock up 圖  2 component
    8. Add component : comment_box.js 一個component  對應一個test.js 檔 Mocha 不會立即的辨識出新增檔案; 不會live reload 所以新增檔案要 Control+ C 再 npm run test:watch 

---
+++ ReduxSimpleStarter/test/components/comment_box_test.js
@@ -0,0 +1,13 @@
+import { renderComponent , expect } from '../test_helper';
+import CommentBox from '../../src/components/comment_box';
+
+describe('CommentBox Tests', ()=>{
+    it('has a test area', () =>{
+
+    });
+
+    it('has a button', ()=>{
+        
+    });
+
+});
