afe3d1fc00527277db14cf436156f1e209b568df
Gillian <jjjgillian@gmail.com>
2017-10-18 15:20:57 +0800

    9.CommentBox Test
    9. CommentBoxTest
    Chai : https://github.com/chaijs/chai-jquery  Use jquery 來指定DOM物件 ex: class value id class
    
    //此小節執行會出現error 因為我們來沒有在 CommentBox 撰寫道textarea

--- ReduxSimpleStarter/test/components/comment_box_test.js
+++ ReduxSimpleStarter/test/components/comment_box_test.js
@@ -3,11 +3,13 @@ import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox Tests', ()=>{
     it('has a test area', () =>{
+        const component = renderComponent(CommentBox);
+        expect(component.find('textarea')).to.exist; // no() , chai will recongnize it  
 
     });
 
     it('has a button', ()=>{
-        
+
     });
 
 });
