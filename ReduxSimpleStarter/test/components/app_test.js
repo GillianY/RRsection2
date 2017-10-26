209cb55dab1fdc02a03f0c2d35ab239493d5cfe0
Gillian <jjjgillian@gmail.com>
2017-10-27 02:55:16 +0800

    12.Expecting child elements
    add commentbox to app ; and also the test

--- ReduxSimpleStarter/test/components/app_test.js
+++ ReduxSimpleStarter/test/components/app_test.js
@@ -3,17 +3,16 @@ import App from '../../src/components/app';  // 觀測目標 root
 
 // use 'describe' to group togather similar tests //scene 
 describe(' Test the App Component', ()=>{
+  let component;
+  
+  beforeEach(()=>{
+      component = renderComponent(App);
+    });
 
-  // Use 'it' to test a single attribute of a target  //TestCase 
-  //to developers on what this block is goning to test right here 
-  it('shows the correct test',() =>{
-
-    //create an instance of app 
-    const component = renderComponent(App);
-    //Use 'expect' to make 'assertion' about a target (here: App)
-    expect(component).to.contain('React simple starter');
-
-  }); 
+  it('shows a comment box',()=>{
+    expect(component.find('.comment-box')).to.exist;
+  });
+  
 
 }); 
 
