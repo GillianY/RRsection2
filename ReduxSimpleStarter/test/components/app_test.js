c20842f02a431b700cdc91db5fb256f95ec3cad0
Gillian <jjjgrace01@gmail.com>
2017-11-02 17:03:09 +0800

    Revert "16. Stub comment line"
    This reverts commit 3eb34c8caaa8bd8973a53d7d67445fce50df74b0.

--- ReduxSimpleStarter/test/components/app_test.js
+++ ReduxSimpleStarter/test/components/app_test.js
@@ -13,9 +13,6 @@ describe(' Test the App Component', ()=>{
     expect(component.find('.comment-box')).to.exist;
   });
   
-  it('shows a comment list',()=>{
-    expect(component.find('.comment-list')).to.exist;
-  });
 
 }); 
 
